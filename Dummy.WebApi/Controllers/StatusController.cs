2d5645ff42520645e13ea40893a3817293215fc3
Karl Atkinson <karl.atkinson@gmail.com>
2017-06-12 11:36:48 +0100

    missing file

---
+++ Dummy.WebApi/Controllers/StatusController.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Web.Http;
+
+namespace Dummy.WebApi.Controllers
+{
+    public class StatusController : ApiController
+    {
+        public dynamic Get()
+        {
+            return new { Status = "Ok", Environment = ConfigurationManager.AppSettings["Environment"] };
+        }
+    }
+}
