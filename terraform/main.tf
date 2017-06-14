2088e29d8173dc546e9fd455cc13ab419654824f
Karl Atkinson <karl.atkinson@gmail.com>
2017-06-14 09:49:16 +0100

    added s3 logging and formatted tf file

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,85 +14,91 @@ resource "aws_elastic_beanstalk_environment" "eb-dotnet-spike-dev" {
 
   setting {
     namespace = "aws:elasticbeanstalk:application"
-    name = "Application Healthcheck URL"
-    value = "/status"
+    name      = "Application Healthcheck URL"
+    value     = "/status"
   }
 
   setting {
     namespace = "aws:elasticbeanstalk:command"
-    name = "DeploymentPolicy"
-    value = "Rolling"
+    name      = "DeploymentPolicy"
+    value     = "Rolling"
   }
 
   setting {
     namespace = "aws:elasticbeanstalk:command"
-    name = "BatchSizeType"
-    value = "Percentage"
+    name      = "BatchSizeType"
+    value     = "Percentage"
   }
 
   setting {
     namespace = "aws:elasticbeanstalk:command"
-    name = "BatchSize"
-    value = "33"
+    name      = "BatchSize"
+    value     = "33"
   }
 
   setting {
     namespace = "aws:autoscaling:updatepolicy:rollingupdate"
-    name = "RollingUpdateType"
-    value = "Health"
+    name      = "RollingUpdateType"
+    value     = "Health"
   }
 
   setting {
     namespace = "aws:autoscaling:updatepolicy:rollingupdate"
-    name = "RollingUpdateEnabled"
-    value = "true"
+    name      = "RollingUpdateEnabled"
+    value     = "true"
   }
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
-    name = "IamInstanceProfile"
-    value = "aws-elasticbeanstalk-ec2-role"
+    name      = "IamInstanceProfile"
+    value     = "aws-elasticbeanstalk-ec2-role"
   }
 
   setting {
     namespace = "aws:autoscaling:asg"
-    name = "MinSize"
-    value = "2"
+    name      = "MinSize"
+    value     = "2"
   }
 
   setting {
     namespace = "aws:autoscaling:asg"
-    name = "MaxSize"
-    value = "4"
+    name      = "MaxSize"
+    value     = "4"
   }
 
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
-    name = "Environment"
-    value = "My really cool dev env"
+    name      = "Environment"
+    value     = "My really cool dev env"
   }
 
   setting {
     namespace = "aws:elasticbeanstalk:environment:process:default"
-    name = "HealthCheckPath"
-    value = "/status"
+    name      = "HealthCheckPath"
+    value     = "/status"
   }
 
   setting {
     namespace = "aws:elb:healthcheck"
-    name = "HealthyThreshold"
-    value = 2
+    name      = "HealthyThreshold"
+    value     = 2
   }
 
   setting {
     namespace = "aws:elb:healthcheck"
-    name = "UnhealthyThreshold"
-    value = 3
+    name      = "UnhealthyThreshold"
+    value     = 3
   }
 
   setting {
     namespace = "aws:elb:healthcheck"
-    name = "Interval"
-    value = 10
+    name      = "Interval"
+    value     = 10
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:hostmanager"
+    name      = "LogPublicationControl"
+    value     = "true"
   }
 }
