074dc5da9c8b431448128e937c1c13bf2d75a993
Karl Atkinson <karl.atkinson@gmail.com>
2017-07-06 09:55:50 +0100

    test

---
+++ Dummy.WebApi.UnitTests/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Dummy.WebApi.UnitTests
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void Testy()
+        {
+            Assert.Fail("test fail");
+        }
+    }
+}
